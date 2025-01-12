39cd3876a42e2f4cf24cd5ed807770cbd9e81ea7
ankitkumargupta <ankitgupta6881@gmail.com>
2025-01-12 19:33:54 +0530

    FormHandling using Hook(useForm)

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
       <form onSubmit={handleSubmit(data=>console.log(data))} className='w-60 h-52 items-center p-4 border border-zinc-400' action="" >
-        <input {...register('name')} className='m-2 border border-zinc-400' type="text" placeholder='Name' />
-        <input {...register('email')} className='m-2 border border-zinc-400' type="email" placeholder='email' />
+        <input {...register('name')} className='m-3 border border-zinc-400' type="text" placeholder='Name' />
+        <input {...register('email')} className='m-3 border border-zinc-400' type="email" placeholder='email' />
         <div>
           <input type="submit" className='m-2 px-3 py-2 rounded-md text-sm bg-zinc-400'/>
         </div>
